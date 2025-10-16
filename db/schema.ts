2418f61ff3697a4a28b31fbd80bdd4c40e660ebe
Peter Morrison <petermorrison@Peters-MacBook-Air.local>
2025-10-15 23:42:56 -0400

    Added results table to schema.

--- db/schema.ts
+++ db/schema.ts
@@ -3,7 +3,7 @@ import { useDBClient } from './client';
 export const useSchema = () => {
   // To-do: Create a npm script to reset DB during development
   // But for now, uncomment the line below to drop the accounts table
-  useDBClient.execSync(`DROP TABLE IF EXISTS accounts;`);
+  // useDBClient.execSync(`DROP TABLE IF EXISTS accounts;`);
 
   useDBClient.execSync(`
     CREATE TABLE IF NOT EXISTS accounts (
@@ -32,5 +32,18 @@ export const useSchema = () => {
       deleted_at TEXT
     );
     CREATE INDEX IF NOT EXISTS lists_id_idx ON lists (id);
+
+    CREATE TABLE IF NOT EXISTS results (
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      lists_id INTEGER NOT NULL,
+      total_words INTEGER NOT NULL,
+      correct INTEGER NOT NULL,
+      incorrect INTEGER NOT NULL,
+      score REAL NOT NULL,
+      created_at TEXT NOT NULL,
+      updated_at TEXT NOT NULL,
+      deleted_at TEXT
+    );
+    CREATE INDEX IF NOT EXISTS results_id_idx ON results (id);
   `);
 };
