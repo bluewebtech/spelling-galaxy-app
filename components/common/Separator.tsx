a2c2fc2c21257c667590a798a14255d86c82c771
Peter Morrison <petermorrison@Peters-MacBook-Air.local>
2026-03-11 21:14:21 -0400

    Corrected casing to K12 words, simplified separator component styles and profile top padding.

--- components/common/Separator.tsx
+++ components/common/Separator.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
 
 export default function Separator() {
-  return <View className="mx-6 my-4 border-2 border-dotted" />;
+  return <View className="mx-6 my-4 border border-solid border-gray-200" />;
 };
